21e616b7124e1624e2b1db9b84dbdd67a225b3b1
Bolu Tejumola <tejumolabolu@gmail.com>
2026-05-05 00:05:22 +0100

    remove

--- firebase.js
+++ firebase.js
@@ -7,15 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCNAQjKkLnbNZbIFb5DBwfqoVBVxE8CyEw",
-  authDomain: "nexus-taskbar.firebaseapp.com",
-  projectId: "nexus-taskbar",
-  storageBucket: "nexus-taskbar.firebasestorage.app",
-  messagingSenderId: "965411029001",
-  appId: "1:965411029001:web:198c75d043d012a3a4f8d8",
-  measurementId: "G-WFQSB19EBS"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
